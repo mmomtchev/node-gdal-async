1400c318b5070045eb12ac32ae0729f041eeaca1
Momtchil Momtchev <momtchil@momtchev.com>
2025-08-01 22:37:02 +0200

    node, why, oh, why, why in 20?

---
+++ .mocharc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  spec: 'test/*.test.ts',
+  require: [
+    'ts-node/register',
+    'tsconfig-paths/register',
+    'test/_common.ts',
+    'test/_hooks.ts'
+  ],
+  reporter: 'tap',
+  timeout: 20000,
+  'v8-expose-gc': true,
+  'node-option':
+    process.versions.node.split('.')[0] == 20 && process.versions.node.split('.')[1] >= 19 ? [
+      'no-experimental-require-module'
+    ] : []
+}
