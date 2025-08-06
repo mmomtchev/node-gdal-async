64bebdce9afb548267c41f2cd1c41718733fcfe2
Momtchil Momtchev <momtchil@momtchev.com>
2025-08-06 15:35:21 +0100

    workaround for some clever back port in 20.19 (#235)
    * node, why, oh, why, why in 20?
    
    * expose GC via node-option

---
+++ .mocharc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  spec: 'test/*.test.ts',
+  require: [
+    'ts-node/register',
+    'tsconfig-paths/register',
+    'test/_common.ts',
+    'test/_hooks.ts'
+  ],
+  reporter: 'tap',
+  timeout: 20000,
+  'node-option':
+    process.versions.node.split('.')[0] == 20 && process.versions.node.split('.')[1] >= 19 ? [
+      'no-experimental-require-module', 'expose-gc'
+    ] : [ 'expose-gc' ]
+}
