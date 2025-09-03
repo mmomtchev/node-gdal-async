82842b8458e9f5fd39b2f1e8d613bff2cbc7acac
arjun.kannan <arjun.kannan@mngsurvey.com.au>
2025-09-03 09:11:56 +0800

    revert the changes made previous as per the PR comment

--- .mocharc.js
+++ .mocharc.js
@@ -1,21 +1,15 @@
 module.exports = {
   spec: 'test/*.test.ts',
   require: [
-    'ts-node/register/transpile-only',
+    'ts-node/register',
     'tsconfig-paths/register',
     'test/_common.ts',
     'test/_hooks.ts'
   ],
   reporter: 'tap',
   timeout: 20000,
-  // Keep Node options minimal and stable; avoid experimental ESM loader
-  'node-option': (() => {
-    const [ majorStr, minorStr ] = process.versions.node.split('.')
-    const major = parseInt(majorStr, 10)
-    const minor = parseInt(minorStr, 10)
-    const opts = [ 'expose-gc' ]
-    // Keep previous workaround for Node 20.19+ to silence require() of ESM warnings
-    if (major === 20 && minor >= 19) opts.unshift('no-experimental-require-module')
-    return opts
-  })()
+  'node-option':
+      process.versions.node.split('.')[0] == 20 && process.versions.node.split('.')[1] >= 19 ? [
+        'no-experimental-require-module', 'expose-gc'
+      ] : [ 'expose-gc' ]
 }
